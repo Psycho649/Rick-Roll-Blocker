d17e37f06a911d1a28462d405c49228281eec4e3
Yasoga Nanayakkarawasam <63655428+Psycho649@users.noreply.github.com>
2021-10-21 21:47:36 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,9 +1,12 @@
-chrome.webRequest.onBeforeRequest.addListener( 
-    function() { 
-        return {cancel: true };
-    },
-    {
-        urls:['https://raw.githubusercontent.com/Psycho649/Rick-Roll-Blocker/main/block.json'] //urls to block
-    },
-    ["blocking"]
-);
+//TODO: Make this work
+
+chrome.webRequest.onBeforeRequest.addListener(info => {
+  function block_urls() { 
+    return {cancel: true };
+}
+{
+  urls: ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://www.youtube.com/watch?v=xvFZjo5PgG0", "https://www.youtube.com/watch?v=iik25wqIuFo", "https://www.youtube.com/watch?v=ub82Xb1C8os", "https://www.youtube.com/watch?v=Yb6dZ1IFlKc", "https://www.youtube.com/watch?v=QB7ACr7pUuE", "https://www.youtube.com/watch?v=8ybW48rKBME", "https://www.thisworldthesedays.com/*"] //urls to block
+}
+["blocking"]
+  
+})
