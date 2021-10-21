81246ac01bcaf37e053ca0844b3bda450c611791
Yasoga Nanayakkarawasam <63655428+Psycho649@users.noreply.github.com>
2021-10-21 12:50:19 +0530

    Update

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
-chrome.webRequest.onBeforeRequest.addListener( //add listener
-    function() { //add function
+chrome.webRequest.onBeforeRequest.addListener( 
+    function() { 
         return {cancel: true };
     },
     {
-        urls:[https://raw.githubusercontent.com/Psycho649/Rick-Roll-Blocker/main/block.json] //urls to block
+        urls:['https://raw.githubusercontent.com/Psycho649/Rick-Roll-Blocker/main/block.json'] //urls to block
     },
     ["blocking"]
 );
